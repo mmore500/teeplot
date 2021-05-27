c31329b1ca82bdabed4a6150f3195aac546ad598
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-05-26 23:02:25 -0400

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mmore500/teeplot',
-    version='0.0.1',
+    version='0.1.0',
     zip_safe=False,
 )
