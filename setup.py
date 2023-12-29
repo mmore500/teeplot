ce4efc5ca323d83377fae88c3801c36f266a93ca
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-12-28 23:39:43 -0500

    Bump version: 0.7.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mmore500/teeplot',
-    version='0.7.0',
+    version='1.0.0',
     zip_safe=False,
 )
