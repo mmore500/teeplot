7b954eed69135c1186111c37a9f5fe68295dae5a
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-12-29 00:00:09 -0500

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mmore500/teeplot',
-    version='1.0.0',
+    version='1.0.1',
     zip_safe=False,
 )
--- teeplot/__init__.py
+++ teeplot/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Matthew Andres Moreno"""
 __email__ = 'm.more500@gmail.com'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
