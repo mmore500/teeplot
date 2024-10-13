cef273dd321a2634b5a3784f6b292c45998c6f7a
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-10-13 13:14:05 -0400

    Upgrade pinned deps

--- requirements/requirements_dev.sh
+++ requirements/requirements_dev.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 for x in "7" "8" "9" "10" "11"; do
-  "python3.${x}" -m piptools compile requirements_dev.in \
-  -v -o requirements_dev-py3${x}.txt
+  "python3.${x}" -m uv pip compile requirements_dev.in \
+  --upgrade -v -o requirements_dev-py3${x}.txt &
 done
+
+wait
