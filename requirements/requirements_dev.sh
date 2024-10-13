4056fffde328a50e6a6f61cdc470a58d6456a073
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-10-13 13:20:31 -0400

    Handle pwd in requirements-dev.sh

--- requirements/requirements_dev.sh
+++ requirements/requirements_dev.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 for x in "8" "9" "10" "11" "12"; do
   "python3.${x}" -m uv pip compile requirements_dev.in \
   --upgrade -v -o requirements_dev-py3${x}.txt &
