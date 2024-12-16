7a138a7936893add1d46f1fd0e0b08116e43e5be
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-12-15 23:11:15 -0500

    fixup! Test over python3.13, too

--- requirements/requirements_dev.sh
+++ requirements/requirements_dev.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-for x in "8" "9" "10" "11" "12"; do
+for x in "8" "9" "10" "11" "12" "13"; do
   "python3.${x}" -m uv pip compile requirements_dev.in \
   --upgrade -v -o requirements_dev-py3${x}.txt &
 done
