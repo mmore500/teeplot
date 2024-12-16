01835111e8f0808d19a3b4a7b057d6fd4bfb9d90
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-12-15 23:12:05 -0500

    Clean up requirements infrastructure

--- requirements/requirements_dev.sh
+++ requirements/requirements_dev.sh
@@ -3,8 +3,9 @@
 cd "$(dirname "$0")"
 
 for x in "8" "9" "10" "11" "12" "13"; do
-  "python3.${x}" -m uv pip compile requirements_dev.in \
-  --upgrade -v -o requirements_dev-py3${x}.txt &
+  python3 -m uv pip compile requirements_dev.in \
+  --python "3.${x}" \
+  --upgrade -o requirements_dev-py3${x}.txt &
 done
 
 wait
