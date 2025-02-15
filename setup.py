8e438426d3bf3a1b193fcdb96a3447df10a55e98
vivaansinghvi07 <singhvi.vivaan@gmail.com>
2025-02-15 18:55:49 -0500

    Bump version: 1.3.0 → 1.3.1

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mmore500/teeplot',
-    version='1.3.0',
+    version='1.3.1',
     zip_safe=False,
 )
