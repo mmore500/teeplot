3f0fa08ada54a0dc9376cad022cad983f790aea5
Vivaan Singhvi <122464874+vivaansinghvi07@users.noreply.github.com>
2025-02-15 17:27:45 -0500

    Update teeplot/teeplot.py
    Co-authored-by: Matthew Andres Moreno <mmore500.login+gpg@gmail.com>

--- teeplot/teeplot.py
+++ teeplot/teeplot.py
@@ -381,7 +381,7 @@ def teed(*args, **kwargs):
 
 
 def validate_teewrap_kwargs(teeplot_kwargs: dict[str, object]):
-    if not all(k.startswith("teeplot") for k in teeplot_kwargs):
+    if not all(k.startswith("teeplot_") for k in teeplot_kwargs):
         raise ValueError(
             "The only keyword arguments passed into the `teewrap` decorator can be teeplot arguments"
         )
